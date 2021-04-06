86be0940d5bcbb5dc42af9aa07c06f581c401a32
KimHyeok <64545173+kimmang94@users.noreply.github.com>
2021-04-06 17:10:45 +0900

    빅오 표기법 2단계 상수 무시
    영향력이 가장큰 대표 항목만 남기고 삭제

---
+++ BIG-O_/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    { 
+        public int Add4(int N)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < N; i++)
+                sum += i;
+
+            for (int i = 0; i < 2 * N; i++)
+                for (int j = 0; j < 2 * N; j++)
+                    sum += 1;
+
+            sum += 1234567;
+
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
